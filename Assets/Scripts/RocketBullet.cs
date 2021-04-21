0551993907057e781d7eff3794602f7cf0f5f64e
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2021-04-22 01:13:08 +0900

    bullet firing

---
+++ Assets/Scripts/RocketBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBullet : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            Vector2 newPos = this.transform.position;
+            GameObject newGameObject = Instantiate(bulletPrefab) as GameObject;
+            newGameObject.transform.position = newPos;
+        }
+    }
+}
---
+++ Assets/Scripts/bulletSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSpeed : MonoBehaviour
+{
+    public float speed = 4;
+
+    void Update()
+    {
+        transform.Translate (0, speed/50, 0);
+    }
+}
