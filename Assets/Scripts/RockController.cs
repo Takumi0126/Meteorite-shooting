751796383d81c70d4d741127ff9a6f0c95de2625
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2021-04-24 02:21:14 +0900

    RockGenerator

---
+++ Assets/Scripts/RockController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour
+{
+    float fallSpeed;
+    float rotSpeed;
+
+    bool showFlag = false;
+
+    void Start () {
+        this.fallSpeed = 0.01f + 0.1f * Random.value;
+        this.rotSpeed = 5f + 3f * Random.value;
+    }
+        
+    void Update () {
+        transform.Translate( 0, -fallSpeed, 0, Space.World);
+        transform.Rotate(0, 0, rotSpeed );
+
+        if (GetComponent<Renderer>().isVisible) {
+			// １回表示されたあと
+			showFlag = true;
+		} else {
+			if (showFlag) {
+				// 画面の外に出て消えたら、削除する
+				Destroy(this.gameObject);
+			}
+		}
+    }
+}
