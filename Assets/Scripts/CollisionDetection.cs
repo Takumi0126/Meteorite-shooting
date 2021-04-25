ae12aa482a3811bdb1c28dbcb3cc2a45e7d06805
Takumi0126 <65918404+Takumi0126@users.noreply.github.com>
2021-04-26 00:13:19 +0900

    CollisionDetection

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public string targetObject;
+
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.name == targetObject) {
+            Destroy(coll.gameObject);
+            Destroy(this.gameObject);
+            Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
